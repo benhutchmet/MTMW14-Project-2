44ad90a902c1e297e13a7a970452307382b7b46f
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-02-23 21:03:55 +0000

    first commit of python files

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+"""Dictionaries containing the constants and paramaters needed for the tasks"""
+
+
+# For the analytic solution
+
+
---
+++ solvers.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.plt as plt
+import math
+from dictionaries import *
+
+# Define the analytical solvers for Task C
