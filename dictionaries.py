129a6687861fb80f20f883144074357e2357aa31
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-02-27 16:28:47 +0000

    updated value of eta to reflect steady state

--- dictionaries.py
+++ dictionaries.py
@@ -10,7 +10,8 @@ params_analytic = {'f0': 10**-4, # coriolis paramater (s^-1)
                    'H': 1000, # resting depth of fluid (m)
                    'tau0': 0.2, # wind stress vector constant (N m^-2)
                    'L': 10**6, # dimensions of square domain (m)
-                   'eta0': 0, # unknown constant of integration - UPDATE //
+                   'eta0': 0.02495, # unknown constant of integration -
+                   # UPDATE //
                    # WITH STEADY STATE ONCE ANALYTICAL SOLUTION HAS BEEN RUN
                    'gridbox_size': 100000, # spacing of square grid cells (m)
                    'x_points': 10, # number of points in the x domain
