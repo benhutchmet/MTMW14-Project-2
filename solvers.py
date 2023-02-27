22c0d1ef0663ae88cc836d8700bacafe5087a056
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-02-27 10:14:00 +0000

    updated to include function for calculating the analytic solution using Mushgrave (1985) methods

--- solvers.py
+++ solvers.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.plt as plt
+import matplotlib.pyplot as plt
 import math
 from dictionaries import *
 
@@ -95,3 +95,83 @@ def analytic_solution(params_analytic):
     Outputs:
     x - array of x values
     y - array of y values
+    u - analytic solution for fluid motion in x-direction
+    v - analytic solution for fluid motion in y-direction
+    eta - analytic solution for the deviation of water surface from its initial level.
+    """
+
+    # extract the number of x and y points from the dictionary
+    x_points = params_analytic['x_points']
+    y_points = params_analytic['y_points']
+
+    # establish the gridbox size
+    gridbox_size = params_analytic['gridbox_size']
+
+    # define arrays for x and y
+    x = np.linspace(0, x_points*gridbox_size, x_points)
+    y = np.linspace(0, y_points*gridbox_size, y_points)
+
+    # extract the constants from the dictionary
+    f0 = params_analytic['f0']
+    beta = params_analytic['beta']
+    g = params_analytic['g']
+    gamma = params_analytic['gamma']
+    rho = params_analytic['rho']
+    H = params_analytic['H']
+    tau0 = params_analytic['tau0']
+    L = params_analytic['L']
+    eta0 = params_analytic['eta0']
+
+    # define the arrays to store u, v and eta results
+    # does the value of v being situated on the x-axis //
+    # and the value of u being situated on the y-axis affect this?
+    u = np.zeros((x_points, y_points))
+    v = np.zeros((x_points, y_points))
+    eta = np.zeros((x_points, y_points))
+
+    # start the analysis by computing epsilon
+    epsilon = epsilon_function_analytic(gamma, L, beta)
+
+    # then compute the a and b coefficients
+    # for use in functions f_1 and f_2
+    a = a_function_analytic(epsilon)
+    b = b_function_analytic(epsilon)
+
+    # define the coefficient containing tau_0
+    # to make code in loops easier to read
+    tau_coeff = tau0 / (np.pi*gamma*rho*H)
+
+    # compute u, v and eta for all values of x and y
+    for i in range(x_points): # i for x
+        for j in range(y_points): # j for y
+
+            # improve readability
+            sin = np.sin
+            cos = np.cos
+            pi = np.pi
+
+            # compute the analytic solutions of //
+            # u, v and eta at [x,y]
+            # what is the deal with j and i indexing here?
+
+            # analytic solution for u[x,y]
+            u[i, j] = -tau_coeff * f1_function_analytic(x[i]/L, a, b) * cos(pi*y[j]/L)
+
+            # analytic solution for v[x,y]
+            v[i, j] = tau_coeff * f2_function_analytic(x[i]/L, a, b) * sin(pi*y[j]/L)
+
+            # analytic solution for eta[x,y]
+            eta[i, j] = eta0 + tau_coeff * (f0*L/g) * (
+                gamma/(f0*pi) * f2_function_analytic(x[i]/L, a, b) * cos(pi*y[j]/L)
+                + 1/pi * f1_function_analytic(x[i]/L, a, b) * (
+                sin(pi*y[j]/L) * (1 + beta*y[j]/f0)
+                + beta*L/(f0*pi) * cos(pi*y[j]/L)
+                    )
+                )
+
+    # return the values for the analytic solution of u, v and eta as well as x and y
+    return u, v, eta, x, y
+
+
+
+
