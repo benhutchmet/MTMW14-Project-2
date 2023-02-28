81d4ad9e48454bfc59c56c627d6e5c32b489bf66
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-02-28 15:31:48 +0000

    dictionaries updated with test value for params_analytic and 0 for params highest res

--- dictionaries.py
+++ dictionaries.py
@@ -10,7 +10,7 @@ params_analytic = {'f0': 10**-4, # coriolis paramater (s^-1)
                    'H': 1000, # resting depth of fluid (m)
                    'tau0': 0.2, # wind stress vector constant (N m^-2)
                    'L': 10**6, # dimensions of square domain (m)
-                   'eta0': 0.02495, # unknown constant of integration -
+                   'eta0': -0.1145, # unknown constant of integration -
                    # UPDATE //
                    # WITH STEADY STATE ONCE ANALYTICAL SOLUTION HAS BEEN RUN
                    'gridbox_size': 100000, # spacing of square grid cells (m)
@@ -45,7 +45,7 @@ params_analytic_highest_res = {'f0': 10**-4, # coriolis paramater (s^-1)
                                 'H': 1000, # resting depth of fluid (m)
                                 'tau0': 0.2, # wind stress vector constant (N m^-2)
                                 'L': 10**6, # dimensions of square domain (m)
-                                'eta0': 0.02495, # unknown constant of integration -
+                                'eta0': 0, # unknown constant of integration -
                                 # updated with value at steady state eta(0,L/2)
                                 'gridbox_size': 5000, # spacing of square grid cells (m)
                                 'x_points': 200, # number of points in the x domain
