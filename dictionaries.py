3aab7951a2aab122c18d1426faac41dbe95ac853
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-02-28 15:52:16 +0000

    dictionary set up for numerical task D started

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,5 @@
+# %%
+
 """Dictionaries containing the constants and paramaters needed for the tasks"""
 
 
@@ -10,7 +12,7 @@ params_analytic = {'f0': 10**-4, # coriolis paramater (s^-1)
                    'H': 1000, # resting depth of fluid (m)
                    'tau0': 0.2, # wind stress vector constant (N m^-2)
                    'L': 10**6, # dimensions of square domain (m)
-                   'eta0': -0.1145, # unknown constant of integration -
+                   'eta0': 0.0074, # unknown constant of integration -
                    # UPDATE //
                    # WITH STEADY STATE ONCE ANALYTICAL SOLUTION HAS BEEN RUN
                    'gridbox_size': 100000, # spacing of square grid cells (m)
@@ -28,7 +30,7 @@ params_analytic_higher_res = {'f0': 10**-4, # coriolis paramater (s^-1)
                                 'H': 1000, # resting depth of fluid (m)
                                 'tau0': 0.2, # wind stress vector constant (N m^-2)
                                 'L': 10**6, # dimensions of square domain (m)
-                                'eta0': 0.02495, # unknown constant of integration -
+                                'eta0': 0.0074, # unknown constant of integration -
                                 # updated with value at steady state eta(0,L/2)
                                 'gridbox_size': 50000, # spacing of square grid cells (m)
                                 'x_points': 20, # number of points in the x domain
@@ -45,7 +47,7 @@ params_analytic_highest_res = {'f0': 10**-4, # coriolis paramater (s^-1)
                                 'H': 1000, # resting depth of fluid (m)
                                 'tau0': 0.2, # wind stress vector constant (N m^-2)
                                 'L': 10**6, # dimensions of square domain (m)
-                                'eta0': 0, # unknown constant of integration -
+                                'eta0': 0.0074, # unknown constant of integration -
                                 # updated with value at steady state eta(0,L/2)
                                 'gridbox_size': 5000, # spacing of square grid cells (m)
                                 'x_points': 200, # number of points in the x domain
@@ -53,3 +55,22 @@ params_analytic_highest_res = {'f0': 10**-4, # coriolis paramater (s^-1)
                                 'u_fig_name': 'u_analytic_highest_res', # name for the u figure
                                 'v_fig_name': 'v_analytic_highest_res', # name for the v figure
                                 'eta_fig_name': 'eta_analytic_highest_res'} # name for the eta figure
+
+
+# For the numerical solution
+
+params_numerical_TaskD_1Day = {'f0': 10**-4, # coriolis paramater (s^-1)
+                                 'beta': 10**-11, # beta value for B-plane approx (m^-1s^-1)
+                                    'g': 10, # gravitational acceleration (ms^-2)
+                                    'gamma': 10**-6, # linear drag coefficient (s^-1)
+                                    'rho': 1000, # uniform density (kg m^-3)
+                                    'H': 1000, # resting depth of fluid (m)
+                                    'tau0': 0.2, # wind stress vector constant (N m^-2)
+                                    'tau_meridional': 0, # meridional wind stress (N m^-2)
+                                    'L': 10**6, # dimensions of square domain (m)
+                                    'gridbox_size': 100000, # spacing of square grid cells (m)
+                                    'x_points': 10, # number of points in the x domain
+                                    'y_points': 10, # number of points in the y domain
+                                    'dt': 480, # time step (s) - ambitious (approx 0.7 below CFL criteria)
+
+# %%
